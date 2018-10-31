7d9bc6a2285266ca7980cef19ef92724b850eb1a
PedroAugustoRamalhoDuarte <pedro_aduarte@aluno.unb.br>
2018-10-31 10:56:22 -0300

    Merge funcional

--- include/header.hpp
+++ include/header.hpp
@@ -2,19 +2,19 @@
 #define HEADER_H
 #include <string>
 
-class Registro{
-	public:
-		char* nome;
-		char turma;
-		char curso;
-		int op;
-		int matric;
-		int chave;
-};
+typedef struct registro{
+	char* nome;
+	char turma;
+	char curso;
+	int op;
+	char* matric;
+	int chave;
+	char* info;
+}Registro;
 
 int merge(char*  arquivo1, char*  arquivo2);
 
-void Ler_lista(FILE* arq, Registro reg);
+void ler_linha_arquivo(FILE* arq, Registro* reg);
 
-void escreve_arquivo(FILE* arq, Registro reg);
+void escreve_arquivo(FILE* arq, Registro* reg);
 #endif // HEADER_H
--- source/intercalacao.cpp
+++ source/intercalacao.cpp
@@ -3,13 +3,22 @@
 #include<string>
 #include "../include/header.hpp"
 
-void escreve_arquivo(FILE* arq, Registro reg){
+void escreve_arquivo(FILE* arq, Registro* reg){
 	//Escrever Registro
+	fprintf(arq, "%s\n", reg->info);
 }
-void Ler_lista(FILE* arq, Registro reg){
+void ler_linha_arquivo(FILE* arq, Registro *reg){
 	char* linha = reinterpret_cast<char*> (malloc(sizeof(char ) * 120));
+	int i;
+	char aux[6];
 	fscanf(arq, "%[^\n]s", linha); /* Pega uma linhas do arquivo */
     fgetc(arq); /* Pegar o \n que sobra */
+    printf("%s\n", linha);
+  	for(i = 0; i < 6; i++){
+  		aux[i] = linha[i];
+  	}
+  	reg->chave = atoi(aux);
+  	reg->info = linha;
 	// Pegar o Registro e a chave
 }
 
@@ -19,20 +28,37 @@ int merge(char*  arquivo1, char*  arquivo2){
 	FILE *arqmerge = fopen("lista12.txt", "w+");
 	if (arq1 != NULL && arq2 != NULL) { /* Se os dois arquivos forem validos */
 		Registro reg1,reg2;
-		Ler_lista(arq1, reg1);
-		Ler_lista(arq2, reg2);
+		ler_linha_arquivo(arq1, &reg1);
+		ler_linha_arquivo(arq2, &reg2);
 		while (!feof(arq1) && !feof(arq2)) { /* Enquanto houver registros*/
+			/* Se acabar algum dos 2
+			if(feof(arq1)){
+				while(!feof(arq2)){
+					escreve_arquivo(arqmerge, &reg2);
+					ler_linha_arquivo(arq2, &reg2);
+				}
+			}else{
+				while(!feof(arq1)){
+					escreve_arquivo(arqmerge, &reg1);
+					ler_linha_arquivo(arq1, &reg1);
+				}
+			}*/
+			/* Se os dois arquivos estiverem */
+			printf("Comparacao:%d , %d \n", reg1.chave,reg2.chave);
 			if (reg1.chave < reg2.chave) {
-				escreve_arquivo(arqmerge, reg1);
-				Ler_lista(arq1, reg1);
+				printf("1 Menor\n");
+				escreve_arquivo(arqmerge, &reg1);
+				ler_linha_arquivo(arq1, &reg1);
 			} else {
 				if (reg2.chave < reg1.chave) {
-					escreve_arquivo(arqmerge, reg2);
-					Ler_lista(arq2, reg2);
+					printf("2 Menor\n");
+					escreve_arquivo(arqmerge, &reg2);
+					ler_linha_arquivo(arq2, &reg2);
 				} else {
-					escreve_arquivo(arqmerge, reg1);
-					Ler_lista(arq1, reg1);
-					Ler_lista(arq2, reg2);
+					printf("Igual\n");
+					escreve_arquivo(arqmerge, &reg1);
+					ler_linha_arquivo(arq1, &reg1);
+					ler_linha_arquivo(arq2, &reg2);
 				}
 			}
 		} 
