307a48576b6f06bbc5bb34a5aae3f954fca515a3
PedroAugustoRamalhoDuarte <pedro_aduarte@aluno.unb.br>
2018-11-01 03:57:19 -0300

    Conta Registros

--- source/indicesec.cpp
+++ source/indicesec.cpp
@@ -1,16 +1,9 @@
-/*
-*
-*
-*
-*
-*
-*
-*
-*
-*/
+#include<list>
+#include<stdio.h>
+#include "../include/header.hpp"
 
 void lista_invertida(const char* nomearq){
-	FILE* arq = fopen(arq, "r");
+	FILE* arq = fopen(nomearq, "r");
 	/* Arquivo com numero de 0 a 9 */
 	char nomeinv[30] = "listainv ";
 	for (int i = 0; i < 30; ++i) {
@@ -20,6 +13,18 @@ void lista_invertida(const char* nomearq){
 			break;
 		}
 	} 
-	FILE* arqinv = fopen(arq, "w+");
-	
+	FILE* arqinv = fopen(nomeinv, "w+");
+	Registro reg;
+	while(!feof(arq)){/* Enquanto houver registros */
+		ler_linha_arquivo(arq, &reg);
+	}
+}
+
+long calcularRegistroArquivo(FILE* arquivo){
+	long posicaoAtual = ftell(arquivo);
+	long tamanho;
+	fseek(arquivo, 0, SEEK_END);
+	tamanho = ftell(arquivo);
+	fseek(arquivo, posicaoAtual, SEEK_SET);
+	return tamanho/TAM_REG;
 }
--- source/teste_inter.cpp
+++ source/teste_inter.cpp
@@ -2,8 +2,18 @@
 #include "../include/catch.hpp"
 #include "../include/header.hpp"
 
+char string1[30] = "lista1.txt";
+char string2[30] = "lista2.txt";
+
 TEST_CASE("merge"){
-	char* string1 = "lista1.txt";
-	char* string2 = "lista2.txt";
 	REQUIRE(merge(string1, string2));
+}
+
+TEST_CASE("Calcula Registro Arquivo"){
+	FILE* arq = fopen(string1, "r");
+	REQUIRE(calcularRegistroArquivo(arq) == 19);
+	fclose(arq);
+	FILE* arq1 = fopen(string2, "r");
+	REQUIRE(calcularRegistroArquivo(arq1) == 22);
+	fclose(arq1);
 }
